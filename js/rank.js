a59518727c15ad78cec50cf558a2bb7afe9a8716
Pliavi <pliavi.rpgm@gmail.com>
2018-12-18 19:42:48 -0200

    [014] - Desafio Intermediário

--- js/rank.js
+++ js/rank.js
@@ -1,7 +1,14 @@
 const $heart = document.querySelector(".heart");
+const $stars = document.querySelectorAll(".star");
 
 $heart.addEventListener('click', function() {
     $heart.classList.toggle('active');
 });
 
+
+$stars.forEach(function($star) {
+    $star.addEventListener('click', function() {
+        $star.classList.toggle('active');
+    });
+})
 
